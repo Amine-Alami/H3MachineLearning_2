39d964fa0c86df7915f08d1d93b9d603dcf72f52
Amine-Alami <alamia502@gmail.com>
2024-03-21 14:00:30 +0100

    fixed some errors

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import streamlit as st
 from PIL import Image
 import numpy as np
 from transformers import TrOCRProcessor, VisionEncoderDecoderModel, VitsModel, AutoTokenizer
-from IPython.display import Audio
 import torch
 import yolov5
 
@@ -29,33 +28,38 @@ def load_tts_model():
 def main():
     st.title("License Plate Recognition App")
 
+    # Upload file
     uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+    
+    # Load models on startup
+    model = load_model()
+    processor, ocr_model = load_ocr_model()
+    tts_model, tokenizer = load_tts_model()
 
     if uploaded_file is not None:
         img = Image.open(uploaded_file)
         st.image(img, caption='Uploaded Image', use_column_width=True)
 
-        model = load_model()
-        processor, ocr_model = load_ocr_model()
-        tts_model, tokenizer = load_tts_model()
-
         if st.button("Run Inference"):
             results = model(img, size=640)
+            # results.show()
             predictions = results.pred[0]
             boxes = predictions[:, :4] # x1, y1, x2, y2
             scores = predictions[:, 4]
             categories = predictions[:, 5]
 
-            # results.show()
-
+            # Crop the image of the license plate
             cropped_image = img.crop(tuple(results.xyxy[0][0, :4].squeeze().tolist()[:4]))
+            st.image(cropped_image, caption='Plate detected')
 
+            # Extract text from the image
             pixel_values = processor(cropped_image, return_tensors="pt").pixel_values
             generated_ids = ocr_model.generate(pixel_values)
             generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
 
             st.write("Detected License Plate Text:", generated_text)
 
+            # Convert the text to audio
             inputs = tokenizer(generated_text, return_tensors="pt")
             with torch.no_grad():
                 output = tts_model(**inputs).waveform
