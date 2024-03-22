a2be5f6fb154a5f1bff469255b6b817d5b18e6dd
Amine-Alami <alamia502@gmail.com>
2024-03-22 10:06:44 +0100

    added test image and readme

---
+++ README.md
@@ -0,0 +1,45 @@
+# License Plate Recognition App
+
+This web application allows users to upload images containing license plates, perform license plate detection and recognition, and convert the recognized text into audio.
+
+## Overview
+
+The app utilizes computer vision models for license plate detection (YOLOv5) and optical character recognition (TR-OCR) to extract text from license plate images. Additionally, it employs a text-to-speech model (VITS) to convert the recognized text into audio, providing an accessible user experience.
+
+![Architecture](screenshots/architecture.png)
+
+## Features
+
+- Upload images containing license plates.
+- Detect license plates in the uploaded images.
+- Recognize text from the detected license plates.
+- Convert the recognized text into audio for accessibility.
+
+## Installation
+
+To run the app locally, follow these steps:
+
+1. Clone the repository:
+
+    ```bash
+    git clone https://github.com/Amine-Alami/H3MachineLearning_2.git
+    ```
+
+2. Navigate to the project directory:
+
+    ```bash
+    cd H3MachineLearning_2
+    ```
+
+3. Install the required dependencies:
+
+    ```bash
+    pip install -r requirements.txt
+    ```
+
+## Usage
+
+To launch the app, run the following command:
+
+```bash
+streamlit run app.py
--- main.py
+++ main.py
@@ -5,19 +5,16 @@ import torch
 import yolov5
 
 # Load YOLOv5 model
-@st.cache(allow_output_mutation=True)
 def load_model():
     return yolov5.load('keremberke/yolov5m-license-plate')
 
 # Load TR-OCR model
-@st.cache(allow_output_mutation=True)
 def load_ocr_model():
     processor = TrOCRProcessor.from_pretrained("microsoft/trocr-base-handwritten")
     model = VisionEncoderDecoderModel.from_pretrained("microsoft/trocr-base-handwritten")
     return processor, model
 
 # Load TTS model
-@st.cache(allow_output_mutation=True)
 def load_tts_model():
     model = VitsModel.from_pretrained("facebook/mms-tts-eng")
     tokenizer = AutoTokenizer.from_pretrained("facebook/mms-tts-eng")
@@ -27,42 +24,49 @@ def load_tts_model():
 def main():
     st.title("License Plate Recognition App")
 
+    # Static test image
+    test_image_path = "test_image.jpg"
+    test_image = Image.open(test_image_path)
+
     # Upload file
     uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
     
-    # Load models on startup
-    model = load_model()
-    processor, ocr_model = load_ocr_model()
-    tts_model, tokenizer = load_tts_model()
-
     if uploaded_file is not None:
         img = Image.open(uploaded_file)
-        st.image(img, caption='Uploaded Image', use_column_width=True)
+    else:
+        img = test_image
 
-        if st.button("Run Inference"):
-            results = model(img, size=640)
-            # results.show()
-            predictions = results.pred[0]
-            boxes = predictions[:, :4] # x1, y1, x2, y2
-            scores = predictions[:, 4]
-            categories = predictions[:, 5]
+    st.image(img, caption='Image', use_column_width=True)
 
-            # Crop the image of the license plate
-            cropped_image = img.crop(tuple(results.xyxy[0][0, :4].squeeze().tolist()[:4]))
-            st.image(cropped_image, caption='Plate detected')
+    if st.button("Run Inference"):
+        # Load models on startup
+        model = load_model()
+        processor, ocr_model = load_ocr_model()
+        tts_model, tokenizer = load_tts_model()
 
-            # Extract text from the image
-            pixel_values = processor(cropped_image, return_tensors="pt").pixel_values
-            generated_ids = ocr_model.generate(pixel_values)
-            generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+        results = model(img, size=640)
+        # results.show()
+        predictions = results.pred[0]
+        boxes = predictions[:, :4] # x1, y1, x2, y2
+        scores = predictions[:, 4]
+        categories = predictions[:, 5]
 
-            st.write("Detected License Plate Text:", generated_text)
+        # Crop the image of the license plate
+        cropped_image = img.crop(tuple(results.xyxy[0][0, :4].squeeze().tolist()[:4]))
+        st.image(cropped_image, caption='Plate detected')
 
-            # Convert the text to audio
-            inputs = tokenizer(generated_text, return_tensors="pt")
-            with torch.no_grad():
-                output = tts_model(**inputs).waveform
-            st.audio(output.numpy(), format="audio/wav", sample_rate=tts_model.config.sampling_rate)
+        # Extract text from the image
+        pixel_values = processor(cropped_image, return_tensors="pt").pixel_values
+        generated_ids = ocr_model.generate(pixel_values)
+        generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+
+        st.write("Detected License Plate Text:", generated_text)
+
+        # Convert the text to audio
+        inputs = tokenizer(generated_text, return_tensors="pt")
+        with torch.no_grad():
+            output = tts_model(**inputs).waveform
+        st.audio(output.numpy(), format="audio/wav", sample_rate=tts_model.config.sampling_rate)
 
 if __name__ == "__main__":
     main()
